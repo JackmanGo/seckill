856553fb9eeea91ab53a806b3ae1d58491934558
wangxi <wangxi950709@gmail.com>
2016-09-04 00:24:19 +0800

    project structure

---
+++ src/main/java/com/redisTest/www/com/wang/seckill/App.java
@@ -0,0 +1,13 @@
+package com.redisTest.www.com.wang.seckill;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
